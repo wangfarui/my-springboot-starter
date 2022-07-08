10282b087e135cc06e5355b923e80245c840ce07
WangFaRui <wangfarui163@163.com>
2022-07-08 18:14:57 +0800

    adjust project framework

---
+++ dependency-framework/web-starter/src/main/java/com/wfr/springboot/dependency/framework/web/starter/DependencyFrameworkWebStarter.java
@@ -0,0 +1,10 @@
+package com.wfr.springboot.dependency.framework.web.starter;
+
+/**
+ * 基础服务依赖框架web层
+ *
+ * @author wangfarui
+ * @since 2022/7/8
+ */
+public final class DependencyFrameworkWebStarter {
+}
