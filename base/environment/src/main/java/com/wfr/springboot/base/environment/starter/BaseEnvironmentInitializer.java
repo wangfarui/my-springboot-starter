e9a01650ca1fa058b4e2111c748849f551b8b512
WangFaRui <wangfarui163@163.com>
2022-07-18 13:56:04 +0800

    update environment module configuration order

--- base/environment/src/main/java/com/wfr/springboot/base/environment/starter/BaseEnvironmentInitializer.java
+++ base/environment/src/main/java/com/wfr/springboot/base/environment/starter/BaseEnvironmentInitializer.java
@@ -2,8 +2,11 @@ package com.wfr.springboot.base.environment.starter;
 
 import com.wfr.springboot.base.environment.BaseEnvironment;
 import lombok.NonNull;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Role;
+import org.springframework.core.Ordered;
 
 /**
  * 基础环境服务初始化器
@@ -11,10 +14,18 @@ import org.springframework.context.ConfigurableApplicationContext;
  * @author wangfarui
  * @since 2022/7/7
  */
-public class BaseEnvironmentInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+@Role(BeanDefinition.ROLE_INFRASTRUCTURE)
+public class BaseEnvironmentInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext>, Ordered {
+
+    public static final int BASE_ENVIRONMENT_INITIALIZER_ORDER = Ordered.HIGHEST_PRECEDENCE;
 
     @Override
     public void initialize(@NonNull ConfigurableApplicationContext applicationContext) {
         BaseEnvironment.setApplicationContext(applicationContext);
+    }
+
+    @Override
+    public int getOrder() {
+        return BASE_ENVIRONMENT_INITIALIZER_ORDER;
     }
 }
--- base/environment/src/main/java/com/wfr/springboot/base/environment/starter/BaseEnvironmentPostProcessor.java
+++ base/environment/src/main/java/com/wfr/springboot/base/environment/starter/BaseEnvironmentPostProcessor.java
@@ -1,8 +1,11 @@
 package com.wfr.springboot.base.environment.starter;
 
 import com.wfr.springboot.base.environment.BaseEnvironment;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.env.EnvironmentPostProcessor;
+import org.springframework.context.annotation.Role;
+import org.springframework.core.Ordered;
 import org.springframework.core.env.ConfigurableEnvironment;
 
 /**
@@ -11,10 +14,18 @@ import org.springframework.core.env.ConfigurableEnvironment;
  * @author wangfarui
  * @since 2022/7/12
  */
-public class BaseEnvironmentPostProcessor implements EnvironmentPostProcessor {
+@Role(BeanDefinition.ROLE_INFRASTRUCTURE)
+public class BaseEnvironmentPostProcessor implements EnvironmentPostProcessor, Ordered {
+
+    public static final int BASE_ENVIRONMENT_POST_PROCESSOR_ORDER = Ordered.HIGHEST_PRECEDENCE;
 
     @Override
     public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
         BaseEnvironment.configEnvironment(environment, application);
+    }
+
+    @Override
+    public int getOrder() {
+        return BASE_ENVIRONMENT_POST_PROCESSOR_ORDER;
     }
 }
